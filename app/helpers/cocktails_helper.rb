b3f081bd2706ddefc8c02ee235185b564c051abc
Jessica Ho <hwyann@gmail.com>
2020-11-06 12:19:23 +0900

    something

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,9 @@
 module CocktailsHelper
+  def cocktail_photo(cocktail)
+    if cocktail.photo.attached?
+      cl_image_path(cocktail.photo.key)
+    else
+      ['https://cf.ltkcdn.net/cocktails/images/orig/259354-2121x1414-whiskey-sour.jpg', 'https://cf.ltkcdn.net/cocktails/images/orig/252120-2121x1414-sazerac.jpg', 'https://cf.ltkcdn.net/cocktails/images/orig/252116-2121x1414-whiskey-sour.jpg'].sample
+    end
+  end
 end
